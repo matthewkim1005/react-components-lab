c0c0f097fd32de46a9b94a21f2e3c03a3e216dbc
matthewkim1005 <matt_hew_kim@yahoo.com>
2024-08-14 09:26:44 -0700

    Fixed error.

--- src/WeatherForcast/WeatherForecast.jsx
+++ src/WeatherForcast/WeatherForecast.jsx
@@ -2,12 +2,12 @@ import './Weather.css';
 import '../App.jsx';
 
 const WeatherForecast = (props) => {
-  const { day, img, imgAlt, condictions, time } = props;
+  const { day, img, imgAlt, conditions, time } = props;
   return (
     <div className="weather">
       <h2>{ day }</h2>
       <img src={ img } alt={ imgAlt } />
-      <p><span>conditions: </span>{ condictions }</p>
+      <p><span>conditions: </span>{ conditions }</p>
       <p><span>time: </span>{ time }</p>
     </div>
   )
